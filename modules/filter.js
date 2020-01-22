af845259e837bd96b1c43fe6e294106a99467295
Roy Kuijper <roykuijper199716@hotmail.com>
2020-01-22 18:46:48 +0100

    detailPage done still bugs when filter is used

--- modules/filter.js
+++ modules/filter.js
@@ -2,7 +2,7 @@
 import data from '../assets/data/test-data.js';
 
 let allCheckboxes = document.querySelectorAll('input[type=checkbox]');
-console.log(allCheckboxes)
+
 let newArray = data.slice();
 // console.log(newArray)
 
@@ -17,14 +17,74 @@ console.log(newArray)
 function showArray(data) {
     document.querySelector('.container').innerHTML = `${data.map(function(shard) {
         return `
-            <a class="article" href="../view/details.html">
+            <button class="article" href="#" value="${shard.ID}">
                 <img src="../assets/images/vase.png"></img>
                 <h3>${shard.ID}</h3>
                 <p>${shard.CONTEXT}</p>
-            </a>
+            </button>
         `
     }).join('')}
     `
+}
+
+function test(value, data) {
+
+    data.map(shard => {
+        if (shard.ID == value) {
+            document.querySelector('.detailsWrap').innerHTML = `
+            <img src="../assets/images/vase.png">
+            <div class="details">
+                <h2>ID</h2>
+                <p>${shard.ID}</p>
+            </div>
+            <div class="details">
+                <h2>Initials Founder</h2>
+                <p>${shard.INITIALS}</p>
+            </div>
+            <div class="details">
+                <h2>Excavation Date</h2>
+                <p>${shard.EXCAVATIONDATE}</p>
+            </div>
+            <div class="details">
+                <h2>Context</h2>
+                <p>${shard.CONTEXT}</p>
+            </div>
+            <div class="details">
+                <h2>Year</h2>
+                <p>${shard.SEASON}</p>
+            </div>
+            <div class="details">
+                <h2>Shape Object</h2>
+                <p>${shard.SHAPEOBJECT}</p>
+            </div>
+            <div class="details">
+                <h2>Shape Details</h2>
+                <p>${shard.SHAPEDETAILS}</p>
+            </div>
+            <div class="details">
+                <h2>Ware</h2>
+                <p>${shard.WARE}</p>
+            </div>
+            <div class="details">
+                <h2>Production Place</h2>
+                <p>${shard.PRODUCTIONPLACE}</p>
+            </div>
+            <div class="details">
+                <h2>Conservation</h2>
+                <p>${shard.CONSERVATION}</p>
+            </div>
+            <div class="details">
+                <h2>Fragments</h2>
+                <p>${shard.FRAGMENTS}</p>
+            </div>
+            <div class="details">
+                <h2>Chronology</h2>
+                <p>${shard.CHRONOLOGY}</p>
+            </div>
+            `
+        } 
+    })
+
 }
 
 function handleChange() {
@@ -35,3 +95,9 @@ function handleChange() {
 function filterBySeason(e) {
     return document.getElementById(e.SEASON).checked;
 }
+
+document.querySelectorAll('.container button').forEach(button => {
+    button.addEventListener('click', () => {
+        test(button.value, data);
+    });
+});
