3b28ebf143c54c20ee52e0fbf256250367da64bb
Roy Kuijper <roykuijper199716@hotmail.com>
2020-01-23 02:06:41 +0100

    got the filters working at overview page

--- main.js
+++ main.js
@@ -6,25 +6,131 @@ let newArray = data.slice();
 
 showArray(newArray);
 
+console.log('data before filtering: ', data)
+
 document.querySelectorAll('input[type=checkbox]').forEach(checkbox => {
     checkbox.addEventListener('change', () => {
-        handleChange()
+
+        //loop through all 'SEASON' checkboxes -> if the checkbox is checked the user wants to filter on the value
+        //all checked checkbox values are put in an array -> every value in seasonFilters is a value the user wants to filter on
+        const seasonFilters = []
+        for (const checkBox of document.querySelectorAll('.styleCheckbox:nth-of-type(1) input[type="checkbox"]')) {
+            if (checkBox.checked === true) {
+                seasonFilters.push(checkBox.value)
+            }
+        }
+
+        //loop through all 'SHAPE' checkboxes -> if the checkbox is checked the user wants to filter on the value
+        //all checked checkbox values are put in an array -> every value in shapeFilters is a value the user wants to filter on
+        const shapeFilters = []
+        for (const checkBox of document.querySelectorAll('.styleCheckbox:nth-of-type(2) input[type="checkbox"]')) {
+            console.log(checkBox)
+            if (checkBox.checked === true) {
+                shapeFilters.push(checkBox.value)
+            }
+        }
+
+        //loop through all 'CHRONOLOGY' checkboxes -> if the checkbox is checked the user wants to filter on the value
+        //all checked checkbox values are put in an array -> every value in chronologyFilters is a value the user wants to filter on
+        const chronologyFilters = []
+        for (const checkBox of document.querySelectorAll('.styleCheckbox:nth-of-type(3) input[type="checkbox"]')) {
+            if (checkBox.checked === true) {
+                chronologyFilters.push(checkBox.value)
+            }
+        }
+
+        //loop through all 'CONSERVATION' checkboxes -> if the checkbox is checked the user wants to filter on the value
+        //all checked checkbox values are put in an array -> every value in conservationFilters is a value the user wants to filter on
+        const conservationFilters = []
+        for (const checkBox of document.querySelectorAll('.styleCheckbox:nth-of-type(4) input[type="checkbox"]')) {
+            if (checkBox.checked === true) {
+                conservationFilters.push(checkBox.value)
+            }
+        }
+
+        //loop through all 'WARE' checkboxes -> if the checkbox is checked the user wants to filter on the value
+        //all checked checkbox values are put in an array -> every value in wareFilters is a value the user wants to filter on
+        const wareFilters = []
+        for (const checkBox of document.querySelectorAll('.styleCheckbox:nth-of-type(5) input[type="checkbox"]')) {
+            if (checkBox.checked === true) {
+                wareFilters.push(checkBox.value)
+            }
+        }
+
+        //create a empty 'filteredData' array -> this array will contain all filteredData when all data is filtered
+        //the condition variable returns all filter arrays with atleast 1 value (these are the arrays that are relevant for the filtering)
+        let filteredData = [],
+            condition = [seasonFilters, shapeFilters, chronologyFilters, conservationFilters, wareFilters].filter(arr => arr.length > 0)
+        //loop through all data items
+        data.forEach(item => {
+            //filterConditions checks how many filter conditions the data-item has passed
+            //each data item will be checked for each 'active' filter (a.k.a for every filter array with atleast 1 value)
+            let filterConditions = 0
+
+
+            if (seasonFilters.length > 0) { //if the user has activated a 'season' filter 
+                if (seasonFilters.includes(String(item.SEASON))) { //check if the data-item is in the seasonFilters array
+                    //include === true --> filterConditions++
+                    //include === false -->don't do anything
+                    filterConditions++
+                }
+            }
+
+            if (shapeFilters.length > 0) {
+                if (shapeFilters.includes(item.SHAPEOBJECT)) { //check if the data-item is in the shapeFilters array
+                    //include === true --> filterConditions++
+                    //include === false -->don't do anything
+                    filterConditions++
+                }
+            }
+
+            if (chronologyFilters.length > 0) {
+                if (chronologyFilters.includes(item.CHRONOLOGY)) { //check if the data-item is in the chronologyFilters array
+                    //include === true --> filterConditions++
+                    //include === false -->don't do anything
+                    filterConditions++
+                }
+            }
+
+            if (conservationFilters.length > 0) {
+                if (conservationFilters.includes(item.CONSERVATION)) { //check if the data-item is in the conservationFilters array
+                    //include === true --> filterConditions++
+                    //include === false -->don't do anything
+                    filterConditions++
+                }
+            }
+
+            if (wareFilters.length > 0) {
+                if (wareFilters.includes(item.WARE)) { //check if the data-item is in the wareFilters array
+                    filterConditions++
+                }
+            }
+
+            //filterConditions = number of filter conditions/checks the data-item has passed
+            //condition.length = number of filter conditions/checks the data-item needs to pass in order to be shown
+            //if the number of passed checks is the same as the number of checks needed to pass (filterConditions === condition.length) 
+            // -> the data-item gets pushed into the filteredData array 
+            if (filterConditions === condition.length) {
+                filteredData.push(item)
+            }
+        })
+        // console.log([seasonFilters, shapeFilters, chronologyFilters, conservationFilters, wareFilters])
+        console.log('data after filtering: ', filteredData)
+
+        handleChange(filteredData)
     });
 });
 
-function handleChange() {
-    newArray = data.filter(filterBySeason);
-    showArray(newArray);
-}
 
-function filterBySeason(e) {
-    return document.getElementById(e.SEASON).checked;
+
+function handleChange(data) {
+    newArray = data;
+    showArray(newArray);
 }
 
 function details(value) {
     newArray.map(shard => {
         if (shard.ID == value) {
-            console.log(shard)
             document.querySelector('.detailsWrap').innerHTML = `
             <img src="../assets/images/vase.png">
             <div class="details">
@@ -82,7 +188,7 @@ function details(value) {
 }
 
 function showArray(data) {
-    document.querySelector('.container').innerHTML = `${data.map(shard => {
+    document.querySelector('.container').innerHTML = `${data.map(function(shard) {
         return `
             <button class="article" href="#" value="${shard.ID}">
                 <img src="../assets/images/vase.png"></img>
@@ -92,6 +198,8 @@ function showArray(data) {
         `
     }).join('')}
     `
+
+
     document.querySelectorAll('.container button').forEach(button => {
         button.addEventListener('click', () => {
             details(button.value);
